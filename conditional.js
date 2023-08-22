530d819f1fa70061b67d40caedebc23734ebc2ee
Nikita Rao <nikitarao1224@gmail.com>
2023-08-22 15:51:01 +0530

    js

--- conditional.js
+++ conditional.js
@@ -29,3 +29,111 @@ if (agee < 33) {
   } else {
     alert("You don't know? ECMAScript!");
   }
+
+  //program...
+  let result={}
+for(let letter of "aabbccaaaccccbbnnnndddsss"){
+    if(result[letter])
+    result[letter]++;
+    else
+result[letter] =1;
+}
+console.log(result)
+console.log(result['a'])
+console.log(result['d'])
+
+
+ //for of use in array....
+ let a =[1,2,3,4]
+    for(let value of a)
+    console.log(value);
+
+  let sum=0;
+  for(let i of a)
+  //console.log(i)
+  sum=sum+i;
+console.log(sum);
+
+for(let letter of "hello")
+console.log(letter)
+
+//for in in Array...
+for (i in a)
+console.log(i)
+for(i in "hello")
+console.log(i)
+console.log(a[2])
+
+
+//object in js...
+let person={
+    name:"nikita",
+    age:20,
+    address:"jaipur",
+    job:"student"
+}
+console.log(person)
+for(element in person)
+console.log(person[element])
+for(element in person)
+console.log(element)
+
+ for( element of Object.keys(person))
+console.log(element)
+for (element of Object.values(person))
+console.log(element)
+
+for([key,value] of Object.entries(person))
+console.log(key,value)
+
+//functions.... 4 types....
+
+function print()
+{
+    console.log("print someting")
+}
+print();
+//factorial...function....
+
+function factorial(x)
+{
+    if(x<=1)
+    return 1;
+else
+return (x*factorial(x-1));
+}
+let fact=factorial(5);
+console.log(fact);
+console.log(factorial(3));
+
+//anamous fun or fun expression or anamonous fun expression
+ const res=function(x) //or function sqr(x)
+{
+    return x*x;
+}
+console.log(res(8));
+
+//arrow function...
+const ans=(x)=>x*x;
+console.log(ans(5));
+
+let add =(x,y)=> {
+    console.log(x+y)
+    return x*y;
+}
+console.log(add(4,8));
+
+//call back functioon...
+let arr=[3,4,1,8,0];
+arr.forEach(i => console.log(i))
+
+//function expression in factorial..
+const fact1 = function factorial1(x) 
+{
+  if(x<=1)
+  return 1;
+else
+    return (x*factorial1(x-1));
+}
+console.log(fact1(5));
+
