89da718c6a573a707b6bb78d05d398bcf1a40427
Nikita Rao <nikitarao1224@gmail.com>
2023-09-03 08:33:02 +0530

    js

---
+++ modal.js
@@ -0,0 +1,20 @@
+"use strict";
+const btnDisplayModal = document.querySelector(".display-modal");
+const modal = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".close-modal");
+
+const showModal = function () {
+  modal.classList.remove("hide");
+  document.querySelector(".blurEffect").classList.remove("hide");
+};
+
+const closeModal = function () {
+  modal.classList.add("hide");
+  document.querySelector(".blurEffect").classList.add("hide");
+};
+
+document.addEventListener("keyup", function (event) {
+  if (event.key === "X") closeModal();
+});
+btnDisplayModal.addEventListener("click", showModal);
+btnCloseModal.addEventListener("click", closeModal);
