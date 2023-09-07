702645f5ee9fd1d31d5c3fb959973135035ed024
Nikita Rao <nikitarao1224@gmail.com>
2023-09-07 19:56:43 +0530

    js

---
+++ calculator.js
@@ -0,0 +1,25 @@
+let string ='';
+let buttons = document.querySelectorAll('input');
+Array.from(buttons).forEach((input)=>{
+    input.addEventListener('onclick',(e)=>
+    {
+        if(e.target.innerHTML == '=')
+        {
+            string =eval(string);
+            document.querySelector('.input').value=string;
+        }
+        else if(e.target.innerHTML == 'AC')
+        {
+         string='';
+         document.querySelector('.input').value=string;
+        }
+        else{
+            string = string + e.target.innerHTML;
+            document.querySelector('.input').value = string;
+        }
+    }
+    )
+
+
+}
+)
